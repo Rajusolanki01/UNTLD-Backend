8c5fea512daee732a926d5f5903b048a35cbba3a
Raju Solanki <92461050+Rajusolanki01@users.noreply.github.com>
2024-04-16 19:04:56 +0530

    Changes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const path = require("path");
 dotenv.config();
 const app = express();
 
-const { UNTLD_BASE_URLL, UNTLD_ADMIN_BASE_URLL } = process.env;
+const { UNTLD_BASE_URLL_NETLIFY, UNTLD_ADMIN_BASE_URLL } = process.env;
 
 //* Setup Middlewares...
 app.use(bodyParser.json());
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("common"));
 app.use(cookieParser());
 const corsOptions = {
-  origin: [UNTLD_BASE_URLL, UNTLD_ADMIN_BASE_URLL],
+  origin: [UNTLD_BASE_URLL_NETLIFY, UNTLD_ADMIN_BASE_URLL],
   credentials: true,
 };
 
